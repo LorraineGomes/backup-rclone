c900117d11c3a43fd94fe0427b913da2cfc6fb8b
LorraineGomes <lorraine@crowtech.digital>
2023-08-22 10:38:11 -0300

    feature/scripts de backup em shell

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # backup
+
+1-Clone o repositório.
+2-Edite o arquivo .sh conforme seu projeto.
+3-Dê permissão a permissão necessária com chmod u+x arquivo.sh
+4-Execute o arquivo com uma cronjob ou ./arquivo.sh
---
+++ backup-arquivos.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# PARTIÇÃO DE DISCO COM RCLONE GDRIVE -> MANDA PARA O GOOGLE DRIVE
+destino="/root/gdrive/"
+
+# Array de sites para backup
+LOJAS+=("loja1")
+LOJAS+=("loja2")
+
+
+for i in "${LOJAS[@]}"
+do
+  TARGET="/var/www/html/$i"
+  cd "$TARGET"
+
+  # Obter a data e hora atual
+  data=$(date +"%Y-%m-%d")
+  hora=$(date +"%H-%M-%S")
+
+  # Nome do arquivo de backup
+  nome="$i-$data-$hora.tar.gz"
+
+  # Criar diretório de destino se não existir
+  mkdir -p "$destino"
+
+  # Compactar a pasta de origem para o diretório de destino, incluindo apenas o .env e a pasta storage
+  find . \( -name '.env' -o -name 'storage' \) -exec tar -czf "$destino/$nome" --transform="s|^./|$i/|" {} +
+
+  # Verificar se o backup foi criado com sucesso
+  if [ $? -eq 0 ]; then
+    echo "Backup completo criado em: $destino/$nome"
+  else
+    echo "Ocorreu um erro ao criar o backup."
+  fi
+
+  # Remover a pasta compactada após 20 dias
+  find "$destino" -name "*.tar.gz" -mtime +20 -exec rm {} \;
+  if [ $? -eq 0 ]; then
+    echo "Pasta compactada será removida após 20 dias."
+  else
+    echo "Ocorreu um erro ao remover a pasta compactada."
+  fi
+
+done
